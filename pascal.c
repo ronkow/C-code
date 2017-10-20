8f4ed2faeb88342a0cc8c043aba4df530a437f51
Ron Kow <ronkow@gmail.com>
2017-10-21 04:17:30 +0800

    Update pascal.c

--- pascal.c
+++ pascal.c
@@ -8,20 +8,16 @@ pascal_triangle(row) returns the entire row for the input row number (0 to 29) o
 #include <stdio.h>
 
 int choose(int n, int k) 
-{
-    int i, nCk = 1;
-    
+{   int i, nCk = 1;
+ 
     if (n>=0 && n<=29 && k>=0 && k<=n)
     {   if (k==0 || k==n)         // includes (n==0 && k==0)
-        {   return 1;
-        }
+            return 1;
         else if (k==1)
-        {   return n;
-        }
+            return n;
         else 
         {   if (k > n/2)
-            {   k = n - k;
-            }
+                k = n - k;
         
             for (i=1; i<=k; i++)
             {   nCk = nCk * n / i;
@@ -31,29 +27,23 @@ int choose(int n, int k)
         }        
     }
     else
-    {
-       if (n < 0 || k < 0 || n > 29 || k > 29)
-       {   printf("n and k must be from 0 to 29, and k must not be larger than n.\n");
-       }
+    {  if (n < 0 || k < 0 || n > 29 || k > 29)
+           printf("n and k must be from 0 to 29, and k must not be larger than n.\n");
        else 
-       {   printf("k must not be larger than n.\n");
-       }
+           printf("k must not be larger than n.\n");
        return 0;
     }
 }
 
 void pascal_triangle(int row) 
-{
-    int i;
+{   int i;
     if (row >= 0 && row <= 29)
     {   for(i = 0; i <= row; i++)            
-        {   printf("%d ",choose(row,i));        
-        }
+            printf("%d ",choose(row,i));        
         printf("\n");
     }
     else
-    {   printf("Row must be from 0 to 29.\n");
-    }
+        printf("Row must be from 0 to 29.\n");
 }
 
 int main(void)
