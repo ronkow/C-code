7419ea83dd3aa630a6208feeaf2e779de1edf3d2
Ron Kow <ronkow@gmail.com>
2017-11-25 22:09:09 +0800

    Update frequency_letters.c

--- frequency_letters.c
+++ frequency_letters.c
@@ -8,23 +8,20 @@ Upper and lower case letters are equivalent and all non-letters are ignored.
 #include <stdio.h>
 
 void freq(char *s)
-{   char letter[27]={'a','b','c','d','e','f','g','h','i','j',
-                    'k','l','m','n','o','p','q','r','s','t',
-                    'u','v','w','x','y','z'};
-    int letter_freq[26]={0};
+{   int letter_freq[26]={0};
      
     for (int i=0; s[i]!='\0'; ++i)
-    {   if ((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z'))  // If character is a letter
-        {   for (int j=0; j<=25; ++j)                              // Scan through letter[] array to find a match
-            {   if (s[i]==letter[j] || (s[i] + 32)==letter[j])     // If match between lowercase s[i] and letter[j] is found, exit loop 
+    {   if ((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z'))  
+        {   for (int j=0; j<=25; ++j)                              
+            {   if (s[i]==j+97 || (s[i] + 32)==j+97)                 
                 {   letter_freq[j] += 1;
                     break;
                 }
             }
         }
     }
-    for (int j=0; letter[j]!='\0'; ++j)
-    {   printf("%c:%d ",letter[j],letter_freq[j]);
+    for (int j=0; j<=25; ++j)
+    {   printf("%c:%d ",j+97,letter_freq[j]);
     }
 }
 
